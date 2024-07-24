f88ddd0f26b4b69c253d9bceb597d9b74d3693e6
SDesya74 <sdesya74@gmail.com>
2024-07-24 13:19:18 +0300

    Updates

---
+++ _app/immutable/chunks/environment.D58AuKNM.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1o7swe0)==null?void 0:s.base)??"/notes";var e;const a=((e=globalThis.__sveltekit_1o7swe0)==null?void 0:e.assets)??t,o="9fb8ee8";export{a,t as b,o as v};
