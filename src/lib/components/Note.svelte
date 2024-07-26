80ac1b090c976929f3576e0f947d58c4473eefa0
SDesya74 <sdesya74@gmail.com>
2024-07-26 17:24:56 +0300

    fix: add prefix to node link in the graph

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { base } from '$app/paths'
     import {messages} from '../index.svelte'
     let { posX, posY, messageContent, onMouseDown, messageId, noteWidth, noteHeight } = $props();
 
@@ -38,7 +39,7 @@
                 <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
             </svg>
         </button>
-        <a class="w-4 h-4 absolute left-8 top-2" href="/#{messageId}">
+        <a class="w-4 h-4 absolute left-8 top-2" href="{base}/#{messageId}">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
                 <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9q-.13 0-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
                 <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4 4 0 0 1-.82 1H12a3 3 0 1 0 0-6z"/>
